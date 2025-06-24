1c7261e51bceadec943d01374d731da41aef4765
PhoneMyatPyaeSone <phonemyatpyaesone8@gmail.com>
2025-06-24 19:25:13 +0700

    Cell4.1 Python ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,7 @@
+age_str = input("Please tell me your age: ")
+current_age = int(age_str)
+
+print(f"You are currently {current_age} years old.")
+print(f"In 10 years, you'll be {current_age + 10} years old.")
+print(f"In 20 years, you'll be {current_age + 20} years old.")
+print(f"In 30 years, you'll be {current_age + 30} years old.")
