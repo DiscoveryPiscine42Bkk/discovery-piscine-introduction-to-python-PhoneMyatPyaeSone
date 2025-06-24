7faec4d8468f5fab2cf426bc2ec5a2c51be36c9d
PhoneMyatPyaeSone <phonemyatpyaesone8@gmail.com>
2025-06-24 19:29:13 +0700

    Cell4.2 Python ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,15 @@
+first_num = int(input("Give me the first number: "))
+second_num = int(input("Give me the second number: "))
+print("Thank you!")
+
+# addition
+print(f"{first_num} + {second_num} = {first_num + second_num}")
+
+# subtraction
+print(f"{first_num} - {second_num} = {first_num - second_num}")
+
+# division 
+print(f"{first_num} / {second_num} = {first_num / second_num}")
+
+# multiplication
+print(f"{first_num} * {second_num} = {first_num * second_num}")
