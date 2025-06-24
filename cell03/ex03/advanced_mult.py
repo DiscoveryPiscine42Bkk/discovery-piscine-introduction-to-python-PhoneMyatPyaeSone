a55c86fc5d6d0e53c55149706531ea950b59e1c7
PhoneMyatPyaeSone <phonemyatpyaesone8@gmail.com>
2025-06-24 17:10:48 +0700

    Cell3.3 Python ex03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i <= 10:
+    j = 0
+    row = []
+    while j <= 10:
+        row.append(str(i * j))
+        j += 1
+    print(f"Table de {i}: {' '.join(row)}")
+    i += 1
